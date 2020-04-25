f93ac12adbfe21f39703f30de445f31d0e90f2a5
arsenitem <arsenitem@bk.ru>
2020-04-25 22:30:03 +0500

    rename folder name

--- backend/src/lib.rs
+++
@@ -1,6 +0,0 @@
-use serde_derive::{Deserialize, Serialize};
-pub mod proto;
-
-pub mod owner;
-pub mod product;
-pub mod manufacturer;
--- backend/src/proto/mod.rs
+++
@@ -1,5 +0,0 @@
-#![allow(bare_trait_objects)]
-pub use self::counterfeit::*;
-
-include!(concat!(env!("OUT_DIR"), "/protobuf_mod.rs"));
-use exonum::crypto::proto::*;
