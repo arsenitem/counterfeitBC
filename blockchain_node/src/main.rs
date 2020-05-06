436d4ccb727be0ce98143ad23f3d5348aa088e46
DenisKochetov <57500456+DenisKochetov@users.noreply.github.com>
2020-05-06 21:25:15 +0500

    fixed build
    Added dependencies to Toml file, changed main function according to exonum's cryptocurrency-advanced 2020 version.

--- blockchain_node/src/main.rs
+++ blockchain_node/src/main.rs
@@ -1,3 +1,25 @@
+<<<<<<< Updated upstream
 fn main() {
     println!("Hello, world!");
 }
+=======
+use exonum_cli::{NodeBuilder, Spec};
+use counterfeitBC::CounterfeitService;
+
+
+use failure::Error;
+use std:: env;
+#[tokio::main]
+async fn main() -> anyhow:: Result<()> 
+{
+
+    exonum::helpers::init_logger()?;
+
+    NodeBuilder::new()
+        .with(Spec:: new(CounterfeitService).with_default_instance())
+        .run()    
+        .await    
+}
+
+
+>>>>>>> Stashed changes
