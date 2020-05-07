cf461b6843a5367404944fb8058df58b8ebe2385
arsenitem <arsenitem@bk.ru>
2020-05-08 00:14:44 +0500

    problem with link exe

--- blockchain_node/src/main.rs
+++ blockchain_node/src/main.rs
@@ -1,9 +1,11 @@
 use exonum_cli::{NodeBuilder, Spec};
 use counterfeitBC::CounterfeitService;
 use failure::Error;
-fn main() -> Result<(), Error> {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     exonum::helpers::init_logger()?;
-    NodeBuilder::development_node()?
-        .with_default_rust_service(CryptocurrencyService)
-        .run()
+    NodeBuilder::new()
+        .with(Spec::new(CounterfeitService).with_default_instance())
+     .run()
+     .await
 }
