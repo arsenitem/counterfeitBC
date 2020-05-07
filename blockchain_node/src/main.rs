304cbc095043d1b718b04b78dc51db5297800924
arsenitem <arsenitem@bk.ru>
2020-05-07 23:12:24 +0500

    edit main

--- blockchain_node/src/main.rs
+++ blockchain_node/src/main.rs
@@ -1,7 +1,9 @@
 use exonum_cli::{NodeBuilder, Spec};
 use counterfeitBC::CounterfeitService;
 use failure::Error;
-fn main() -> Result<()> {
-    exonum::helpers::init_logger();
-    NodeBuilder::development_node().with_default_rust_service(CounterfeitService).run();
+fn main() -> Result<(), Error> {
+    exonum::helpers::init_logger()?;
+    NodeBuilder::development_node()?
+        .with_default_rust_service(CryptocurrencyService)
+        .run()
 }
